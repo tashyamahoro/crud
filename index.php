760143327e4f2d2b9d754aa144853f7aa042dc54
tashyamahoro <tashyamahoro2008@gmail.com>
2022-01-04 06:21:11 -0800

    tambah data

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<?php
+    //Koneksi Database
+    $server = "db4free.net";
+    $user = "tashya20";
+    $pass = "tashyamahoro2008";
+    $database = "double_t";
+
+    $koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
+
+    //jika tombol simpan diklik
+    if(isset($_POST['bSimpan']))
+    {
+        $simpan = mysqli_query($koneksi,"INSERT INTO tkwp (nik, nama, tanggal_lahir, alamat)
+                                        VALUES ('$_POST[tNIK]',
+                                               '$_POST[tNama]',
+                                               '$_POST[tTanggal_Lahir]',
+                                               '$_POST[tAlamat]')
+                                        ");
+        if($simpan) //jika simpan sukses
+        {
+            echo "<script>
+                    alert('Simpan data sukses!');
+                    document.location= 'index.php';
+                 </script>";
+        }
+        else
+        {
+            echo "<script>
+            alert('Simpan data GAGAL!!');
+            document.location= 'index.php';
+         </script>"; 
+        }                       
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -39,6 +74,46 @@
         </form>
     </div>
     </div>
+
+    <!-- Awal Card Tabel -->
+    <div class="card mt-3">
+    <div class="card-header bg-success text-white ">
+        Daftar Anggota KWP 6
+    </div>
+    <div class="card-body">
+
+        <table class= "table table-bordered table-striped">
+            <tr>
+                <th>No.</th>
+                <th>NIK</th>
+                <th>Nama</th>
+                <th>Tanggal Lahir</th>
+                <th>Alamat</th>
+                <th>Aksi</th>
+            </tr>
+            <?php
+                $no = 1;
+                $tampil = mysqli_query($koneksi,"SELECT * from tkwp order by id_kwp desc");
+                while($data = mysqli_fetch_array($tampil)) :
+            ?>
+            <tr>
+                <td><?=$no++;?></td>
+                <td><?=$data ['nik']?></td>
+                <td><?=$data ['nama']?></td>
+                <td><?=$data ['tanggal_lahir']?></td>
+                <td><?=$data ['alamat']?></td>
+                <td>
+                    <a href="#" class="btn btn-warning"> Edit </a>
+                    <a href="#" class="btn btn-danger"> Hapus </a>
+
+                </td>
+            </tr>
+        <?php endwhile; //penutup perulangan while ?>
+        </table>
+        
+    </div>
+    </div>
+    <!-- Akhir Card Tabel-->
 
 </div>
 
